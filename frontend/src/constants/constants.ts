9421eb5a4ebeb43c1536475067c0f0d6b4fbf2ff
Afanasi <narik.fara@mail.ru>
2025-05-16 20:37:08 +0300

    add url

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'none' : 'http://localhost:3000'
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://cv-app-1.onrender.com"
+    : "http://localhost:3000"
